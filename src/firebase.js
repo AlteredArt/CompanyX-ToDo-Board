921bf1af8b1b21c0f8c1e1a275d69437a0245b7f
Altered Art <cherokeedreemer@gmail.com>
2020-08-31 14:38:42 -0600

    finished with logging in

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAPNEvubgxrZ0BSazDKqxIxNqBYz3_ItdM",
+    authDomain: "companyx-todo-board.firebaseapp.com",
+    databaseURL: "https://companyx-todo-board.firebaseio.com",
+    projectId: "companyx-todo-board",
+    storageBucket: "companyx-todo-board.appspot.com",
+    messagingSenderId: "784034461222",
+    appId: "1:784034461222:web:01bdce5d1135dc25f298e7"
+  };
+
+
+  export const firebaseApp = firebase.initializeApp(config)
